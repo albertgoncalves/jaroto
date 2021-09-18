04675aa7f8cec52ccd0a7fac3fe67ca105da524a
albertgoncalves <goncalves.albert@gmail.com>
2021-09-17 22:41:05 -0400

    more OO

--- src/LazyFibs.java
+++ src/LazyFibs.java
@@ -52,15 +52,38 @@ class List<T> {
 public class LazyFibs {
     public static List<Long> fibs;
 
-    static List<Long> zipSum(List<Long> a, List<Long> b) {
-        return new List<Long>(a.head() + b.head(),
-                              () -> zipSum(a.tail(), b.tail()));
+    static List<Long> zipSum(List<Long> xs, List<Long> ys) {
+        return new List<Long>(xs.head() + ys.head(), new F0(xs, ys));
+    }
+
+    static class F0 implements Func<List<Long>> {
+        List<Long> xs;
+        List<Long> ys;
+
+        public F0(List<Long> l0, List<Long> l1) {
+            xs = l0;
+            ys = l1;
+        }
+
+        public List<Long> eval() {
+            return zipSum(xs.tail(), ys.tail());
+        }
+    }
+
+    static class F1 implements Func<List<Long>> {
+        public List<Long> eval() {
+            return zipSum(fibs, fibs.tail());
+        }
+    }
+
+    static class F2 implements Func<List<Long>> {
+        public List<Long> eval() {
+            return new List<Long>(1L, new F1());
+        }
     }
 
     public static void main(String[] args) {
-        fibs = new List<Long>(
-            0L,
-            () -> new List<Long>(1L, () -> zipSum(fibs, fibs.tail())));
+        fibs = new List<Long>(0L, new F2());
         Long x = fibs.drop(50).head();
         System.out.println(x);
         assert x == 12586269025L;
