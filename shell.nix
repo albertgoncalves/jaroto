6e6618b523b8ac8b3aa35019a64ebe2efa88edc9
albertgoncalves <goncalves.albert@gmail.com>
2025-04-10 23:33:40 -0400

    update nix dev env

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
-mkShell.override { stdenv = llvmPackages_17.stdenv; } {
+mkShell.override { stdenv = llvmPackages_19.stdenv; } {
     buildInputs = [
         openjdk
         shellcheck
