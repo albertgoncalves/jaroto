75c590bfb86b9cfdd2c23b6fc5e7084ecb298d51
albertgoncalves <goncalves.albert@gmail.com>
2026-09-05 21:23:08 -0400

    update hello-world

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,5 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+void main(String[] args) {
+    for (int i = 0; i < args.length; ++i) {
+        System.out.println(args[i]);
     }
 }
